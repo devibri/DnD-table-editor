ce1a2199cc47b775b5544c54c0430fd389535710
Devi Acharya <dacharya64@gmail.com>
2020-11-24 10:31:01 -0800

    adding basic character editor

---
+++ example/src/app/Demo.js
@@ -0,0 +1,234 @@
+import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiEditableTable from "mui-editable-table";
+
+class Demo extends Component {
+
+  render() {
+      const shouldBeReadOnly = function(rowData) {
+          return rowData['title'] != 'Mrs';
+      };
+
+      const colSpec = [
+          {title: 'Tag', fieldName: 'tag', inputType: "TextField", width: 200},
+          {title: 'Name', fieldName: 'name', inputType: "TextField", width: 200},
+          {title: 'Occupation', fieldName: 'occupation', inputType: "TextField", width: 300},
+          {title: 'Status', fieldName: 'status', inputType: "SelectField", selectOptions: ["alive", "dead", "unconscious"], width: 200, defaultValue: 'alive'},
+          {title: 'Information', fieldName: 'information', inputType: "TextField", width: 500}
+      ];
+
+      const rowData = [
+         {
+    "tag": "ToblinStonehill",
+      "name": "Toblin Stonehill",
+      "occupation": "Innkeeper",
+      "status": "alive",
+      "information": ["RedbrandHangout"]
+    
+  },
+  {
+    "tag": "ElmarBarthen",
+    
+      "name": "Elmar Barthen",
+      "occupation": "Owns trading post",
+      "status": "alive",
+      "information": ["RedbrandShakedown"]
+    
+  },
+  {
+    "tag": "DarenEdermath",
+      "name": "Daren Edermath",
+      "occupation": "Retired adventurer",
+      "faction": "Order of the Gauntlet",
+      "status": "alive",
+      "information": ["RedbrandHideoutLocation", "GetRidOfRedbrands", "DiggingOldOwlWell"]
+    
+  },
+  {
+    "tag": "LineneGraywind",  
+      "name": "Linene Graywind",
+      "occupation": "Runs trading post",
+      "status": "alive",
+      "information": []
+    
+  }, 
+  {
+    "tag": "HaliaThornton",
+      "name": "Halia Thorton",
+      "occupation": "Runs Phandalin Miner's Exchange",
+      "faction": "Zhentarim",
+      "status": "alive",
+      "information": ["GoblinCragmawCastle", "KillRedbrandLeader"]
+    
+  },  
+  {
+    "tag": "QellineAlderleaf",
+      "name": "Qelline Alderleaf",
+      "occupation": "Halfling farmer",
+      "status": "alive",
+      "information": ["ReidothLocation"], 
+      "familyOf": "Mother of Carp",
+      "friendOf": "Reidoth"
+    
+  },  
+  {
+   "tag": "SisterGaraele",
+      "name": "Sister Garaele",
+      "occupation": "Elf cleric of Tymora",
+      "faction": "Harper",
+      "status": "alive",
+      "information": ["AskAgathaAboutBook"]
+    
+  },  
+  {
+   "tag": "HarbinWester",
+    "name": "Harbin Wester",
+    "occupation": "Townmaster of Phandalin",
+    "status": "alive",
+    "information": ["OrcsTriboarTrail", "TownmasterAndRedbrands"]
+    
+  },  
+  {
+   "tag": "SildarHallwinter",
+    "name": "Sildar Hallwinter",
+    "faction": "Lords' Alliance",
+    "status": "alive",
+    "information": ["MissingIarno"]
+    
+  }, 
+  {
+   "tag": "Narth",
+    "name": "Narth",
+    "occupation": "Farmer",
+    "status": "alive",
+    "information": ["SisterGaraeleExhausted"]
+    
+  },
+  {
+   "tag": "TheRedbrands",
+    "name": "The Redbrands",
+    "faction": "Redbrands",
+    "status": "alive",
+    "information": []
+    
+  },
+  {
+   "tag": "Elsa",
+    "name": "Elsa",
+    "status": "alive",
+    "information": []
+    
+  },
+  {
+   "tag": "Lanar",
+    "name": "Lanar",
+    "status": "alive",
+    "information": ["OrcsTriboarTrail"]
+    
+  },
+  {
+   "tag": "Trilena",
+    "name": "Trilena",
+    "status": "alive",
+    "information": ["DendrarFamilyKidnapped"]
+    
+  },
+  {
+   "tag": "Pip",
+    "name": "Pip",
+    "status": "alive",
+    "information": ["SecretTunnelKnowledge"], 
+    "friendOf": "Carp"
+    
+  },
+  {
+   "tag": "Freda",
+    "name": "Freda",
+    "status": "alive",
+    "information": ["RedbrandsAndHalia"]
+    
+  },
+  {
+   "tag": "Ander",
+    "name": "Ander",
+    "status": "alive",
+    "information": []
+    
+  },
+  {
+   "tag": "Thistle",
+    "name": "Thistle",
+    "status": "alive",
+    "information": []
+    
+  },
+  {
+   "tag": "Grista",
+    "name": "Grista",
+    "status": "alive",
+    "information": []
+    
+  },
+  {
+   "tag": "Carp",
+    "name": "Carp",
+    "status": "alive",
+    "information": ["RedbrandHideoutLocation", "SecretTunnelLocation"],
+    "familyOf": "Child of Qelline Alderleaf"
+    
+  },
+  {
+   "tag": "Agatha",
+    "name": "Agatha the Banshee",
+    "status": "alive",
+    "information": ["AgathaInfo"]
+    
+  },
+  {
+   "tag": "Reidoth",
+    "name": "Reidoth",
+    "status": "alive",
+    "information": ["GreenDragon"]
+    
+  },
+  {
+   "tag": "GundrenRockseeker",
+    "name": "Gundren Rockseeker",
+    "status": "alive",
+    "information": ["WaveEchoCaveMap"]
+    
+  },
+  {
+   "tag": "Hamun",
+    "name": "Hamun the Necromancer",
+    "status": "alive",
+    "information": ["HamunQuest", "FinishHamunQuest"]
+    
+  },
+  {
+   "tag": "RedbrandMinions",
+    "name": "Goblin Droop and Bugbears",
+    "status": "alive",
+    "information": ["RedbrandMinionInfo"]
+    
+  }
+      ];
+
+      const onChange = (dataTable) => {
+          console.log(dataTable)
+      };
+
+      return (
+      <MuiThemeProvider>
+          <MuiEditableTable
+              colSpec={colSpec}
+              rowData={rowData}
+              onChange={onChange}
+              reorderable={true}
+          />
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default Demo;
